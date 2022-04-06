3c524563472a661e333d2717fc8c7680412b8f25
Vlad.Savitskiy <savitskii@skbkontur.ru>
2022-04-06 18:23:16 +0500

    Верстка главного меню

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace TestsGenerator.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCollapseButtonClick(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void OnExitButtonClick(object sender, RoutedEventArgs e) => Environment.Exit(0);
+    }
+}
